2be51c3b824797e46142520daee07a8c7eb7bbbb
Sabrina <pereira.tsabrina@gmail.com>
2023-03-24 23:49:03 -0300

    TT: selected letter and words functionality added

--- index.js
+++ index.js
@@ -1,32 +1,69 @@
 const startButton = document.getElementById('startButton');
 const progressBar = document.querySelector('#progressBar div')
-const correctLetters = document.querySelector('#correctLetters span')
-const errors = document.querySelector('#errors span')
-const wpm = document.querySelector('#wpm span')
 const final = document.querySelector('#final')
 const restartButton = document.querySelector('#final button')
+const wordContainer = document.getElementById('currentWord')
+const input = document.querySelector('input');
+let correctLetters = document.querySelector('#correctLetters span')
+let errors = document.querySelector('#errors span')
+let wpm = document.querySelector('#wpm span')
 
 const gameTime = 2;
+let letterList = [];
+let currentIndex;
 
 function start(){
+  correctLetters = 0;
+  errors = 0;
+  wpm = 0;
   console.log('Start')
   final.classList.toggle('hidden',true)
   progressBar.classList.toggle('completeTime',true)
   startButton.classList.toggle('hidden',true)
 }
 
+function newWord(){
+  if(letterList.length > 0) letterList.forEach(letter => {wordContainer.removeChild(letter)});
+  const nrWordSelected = Math.floor(Math.random()*(wordsArray.length));
+  const selectedWord = wordsArray[nrWordSelected]
+  letterList = []
+  currentIndex = 0
+  for (let i = 0; i < selectedWord.length; i++) {
+    const letter = selectedWord[i];
+    const letterElement = document.createElement('span')
+    letterElement.textContent = selectedWord[i]
+    wordContainer.appendChild(letterElement)
+    letterList.push(letterElement)
+  }
+}
 
 startButton.addEventListener('click', () => start())
+restartButton.addEventListener('click', () => start())
 
 progressBar.addEventListener('animationend', () => {
   final.classList.toggle('hidden',false)
-  console.log('Time ended')
   progressBar.classList.toggle('completeTime',false)
   correctLetters.textContent = 'change'
   errors.textContent = 'change'
   wpm.textContent = 'change'
 })
 
-restartButton.addEventListener('click', () => start())
-
+input.focus();
 document.documentElement.style.setProperty('--time',gameTime+'s')
+newWord()
+
+input.addEventListener('input', (e)=>{
+  if(e.data === letterList[currentIndex].textContent) {
+    currentIndex++
+    correctLetters++;
+    if(currentIndex === letterList.length){
+      newWord()
+    }
+  }else{
+    errors++;
+  }
+
+
+
+})
+input.addEventListener('blur', ()=> input.focus())
